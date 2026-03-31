bf75943637e2d250ad6c16ae7753fe02cd61e6cd
Sankalp Singh <sankalpsinghcoder@gmail.com>
2026-03-31 13:42:39 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,1 +1,25 @@
+# tools.py
 
+import requests
+import os
+
+
+# -------- NEWS TOOL --------
+def get_news():
+    return "News feature coming soon 📰"
+
+
+# -------- FILE WRITE --------
+def write_file(text):
+    with open("notes.txt", "a") as f:
+        f.write(text + "\n")
+    return "Saved to file."
+
+
+# -------- FILE READ --------
+def read_file():
+    if not os.path.exists("notes.txt"):
+        return "No file found."
+
+    with open("notes.txt", "r") as f:
+        return f.read()
